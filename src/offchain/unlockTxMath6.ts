771b87590597aa0528b8f2aa06f93ca88014fe81
Travis Carney <public@netcaffeine.com>
2023-08-12 21:25:16 -0700

    Updated math score criteria for 5th and 6th grade.

--- src/offchain/unlockTxMath6.ts
+++ src/offchain/unlockTxMath6.ts
@@ -1,9 +1,4 @@
-import {
-	Address,
-	DataB,
-	isData,
-	Tx,
-} from '@harmoniclabs/plu-ts';
+import { Address, DataB, isData, Tx } from '@harmoniclabs/plu-ts';
 import { uint8ArrayEq, fromAscii } from '@harmoniclabs/uint8array-utils';
 import { BrowserWallet } from '@meshsdk/core';
 import getTxBuilder from './getTxBuilder';
@@ -11,18 +6,17 @@ import { koios } from './koios';
 import { toPlutsUtxo } from './mesh-utils';
 import { script, scriptAddr } from '../../contracts/mipmath6';
 
-async function getUnlockTx( wallet: BrowserWallet, score: string ): Promise<Tx>
-{
-	const myAddrs = (await wallet.getUsedAddresses()).map( Address.fromString );
-	
+async function getUnlockTx(wallet: BrowserWallet, score: string): Promise<Tx> {
+	const myAddrs = (await wallet.getUsedAddresses()).map(Address.fromString);
+
 	const txBuilder = await getTxBuilder();
-	const myUTxOs = (await wallet.getUtxos()).map( toPlutsUtxo );
+	const myUTxOs = (await wallet.getUtxos()).map(toPlutsUtxo);
 	// const scoreStr = fromAscii( 's' + score );
 	const scoreInt = Number(score);
 
 	/**
 	 * Wallets may have multiple addresses;
-	 * 
+	 *
 	 * to understand which one we previously used to lock funds
 	 * we'll get the address based on the utxo that keeps one of our
 	 * public key hashes as datum
@@ -30,29 +24,27 @@ async function getUnlockTx( wallet: BrowserWallet, score: string ): Promise<Tx>
 	let myAddr!: Address;
 
 	// only the ones with valid datum
-	const utxoToSpend = (await koios.address.utxos( scriptAddr ))
-	.find( utxo => {
+	const utxoToSpend = (await koios.address.utxos(scriptAddr)).find((utxo) => {
 		const datum = utxo.resolved.datum;
 
 		if (
 			// datum is inline
-			isData( datum ) &&
+			isData(datum) &&
 			// and is only bytes
 			datum instanceof DataB
-		)
-		{
+		) {
 			const pkh = datum.bytes.toBuffer();
 
 			// search if it corresponds to one of my public keys
-			const myPkhIdx = myAddrs.findIndex(
-				addr => uint8ArrayEq( pkh, addr.paymentCreds.hash.toBuffer() )
+			const myPkhIdx = myAddrs.findIndex((addr) =>
+				uint8ArrayEq(pkh, addr.paymentCreds.hash.toBuffer())
 			);
 
 			// not a pkh of mine; not an utxo I can unlock
-			if( myPkhIdx < 0 ) return false;
+			if (myPkhIdx < 0) return false;
 
 			// else fund my locked utxo
-			myAddr = myAddrs[ myPkhIdx ];
+			myAddr = myAddrs[myPkhIdx];
 
 			return true;
 		}
@@ -60,13 +52,12 @@ async function getUnlockTx( wallet: BrowserWallet, score: string ): Promise<Tx>
 		return false;
 	});
 
-	if (utxoToSpend === undefined)
-	{
+	if (utxoToSpend === undefined) {
 		throw 'Are you sure your tx had enough time to get to the blockchain?';
 	}
 
 	// TODO: temp code for test run. this logic will ultimately live in the contract
-	const scoreBoolStr = scoreInt >= 61 ? 'true' : 'false';
+	const scoreBoolStr = scoreInt >= 73 ? 'true' : 'false';
 	return txBuilder.buildSync({
 		inputs: [
 			{
@@ -75,27 +66,29 @@ async function getUnlockTx( wallet: BrowserWallet, score: string ): Promise<Tx>
 				inputScript: {
 					script,
 					datum: 'inline', // the datum is present already on `utxoToSpend`
-					redeemer: new DataB( fromAscii(scoreBoolStr) ),
+					redeemer: new DataB(fromAscii(scoreBoolStr)),
 					// redeemer: scoreInt.toFixed(0), // match the redeemer
-				}
-			}
+				},
+			},
 		],
-		requiredSigners: [ myAddr.paymentCreds.hash ],
+		requiredSigners: [myAddr.paymentCreds.hash],
 		// include collateral when using contracts
-		collaterals: [ myUTxOs[0] ],
+		collaterals: [myUTxOs[0]],
 		// send everything back to us
-		changeAddress: myAddr
+		changeAddress: myAddr,
 	});
 }
 
-export async function unlockTx( wallet: BrowserWallet, score: string ): Promise<string>
-{
-	const unsingedTx = await getUnlockTx( wallet, score );
+export async function unlockTx(
+	wallet: BrowserWallet,
+	score: string
+): Promise<string> {
+	const unsingedTx = await getUnlockTx(wallet, score);
 
 	const txStr = await wallet.signTx(
 		unsingedTx.toCbor().toString(),
 		true // partial sign because we have smart contracts in the transaction
 	);
 
-	return (await koios.tx.submit( Tx.fromCbor( txStr ) )).toString();
+	return (await koios.tx.submit(Tx.fromCbor(txStr))).toString();
 }
