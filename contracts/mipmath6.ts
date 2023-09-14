cbfec90ac1323961bc4c2f19816a83b5d7ecb1f2
Travis Carney <public@netcaffeine.com>
2023-09-14 08:20:05 -0700

    Contract code cleanup. No functional change.

--- contracts/mipmath6.ts
+++ contracts/mipmath6.ts
@@ -3,49 +3,37 @@ import {
 	PPubKeyHash,
 	PScriptContext,
 	PaymentCredentials,
-	pBool,
 	Script,
 	bool,
 	compile,
 	makeValidator,
 	pfn,
-	data,
 	bs,
 } from '@harmoniclabs/plu-ts';
 import { network } from '../src/offchain/config';
 
-const contract = pfn([
-	PPubKeyHash.type,
-	// data,
-	bs,
-	PScriptContext.type,
-], bool)
-(( owner, score, ctx ) => {
+const contract = pfn(
+	[PPubKeyHash.type, bs, PScriptContext.type],
+	bool
+)((owner, score, ctx) => {
 	// TODO: for testing, score is currently passed in as "true" or "false"
 	// a score of 61 or greater must be submitted for the contract to succeed
-	// const minimumScore = 61;
-	// const numericScore = Number(score); //(score.toString().slice(1));
-	const signedByOwner = ctx.tx.signatories.some( owner.eqTerm );
-	//const tbPass = score.toString() === 'true';
+	const signedByOwner = ctx.tx.signatories.some(owner.eqTerm);
 	const tbPass = score.eq('true');
-	
-	// return pBool( numericScore >= minimumScore ).and( signedByOwner );
-	return tbPass.and( signedByOwner );
+
+	return tbPass.and(signedByOwner);
 });
 
 /**
  * Utilities
  */
-export const untypedValidator = makeValidator( contract );
+export const untypedValidator = makeValidator(contract);
 
-export const compiledContract = compile( untypedValidator );
+export const compiledContract = compile(untypedValidator);
 
-export const script = new Script(
-	'PlutusScriptV2',
-	compiledContract
-);
+export const script = new Script('PlutusScriptV2', compiledContract);
 
 export const scriptAddr = new Address(
 	network,
-	PaymentCredentials.script( script.hash )
+	PaymentCredentials.script(script.hash)
 );
